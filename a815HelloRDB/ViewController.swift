4cb650989d43cd614da314dbfffa5f38e08bda53
Danny Shen <shenfive@hotmail.com>
2021-08-29 15:03:00 +0800

    Initial Commit

---
+++ a815HelloRDB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a815HelloRDB
+//
+//  Created by 申潤五 on 2021/8/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
