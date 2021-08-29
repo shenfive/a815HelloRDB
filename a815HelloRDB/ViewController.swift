3f3d9340581b08d892867f70c0b0a34977e7a999
Danny Shen <shenfive@hotmail.com>
2021-08-29 15:23:26 +0800

    snap

--- a815HelloRDB/ViewController.swift
+++ a815HelloRDB/ViewController.swift
@@ -6,12 +6,37 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    var dbRef:DatabaseReference!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dbRef = Database.database().reference()
+        
+        Auth.auth().signInAnonymously(completion: nil)
+        Auth.auth().addStateDidChangeListener { auth, user in
+            if let user = user{
+                print("SingIned")
+                print(user.uid)
+            }else{
+                print("SignOut")
+            }
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        let verRef = dbRef.child("appdefult").child("currentVer")
+        
+        verRef.observeSingleEvent(of: .value) { snapshot in
+            print("ver: \(snapshot.value as! String)")
+        }
+        
     }
 
 
