7026db24595da82fd1a558555946df9ead425189
Danny Shen <shenfive@hotmail.com>
2021-09-05 10:17:00 +0800

    New Comment

--- a815HelloRDB/Page3ViewController.swift
+++ a815HelloRDB/Page3ViewController.swift
@@ -6,19 +6,50 @@
 //
 
 import UIKit
+import Firebase
 
 class Page3ViewController: UIViewController {
     
     var nickName = ""
     var key = ""
     var subJect = ""
+    var dbReference:DatabaseReference!
+
+    @IBOutlet weak var inputTextTF: UITextField!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         print("page3 nickName\(nickName)")
         self.title = subJect
-        // Do any additional setup after loading the view.
+        
+        dbReference = Database.database().reference().child("comment").child(key)
+        
+        
     }
+    
+    
+    @IBAction func newComment(_ sender: Any) {
+        let comment = inputTextTF.text ?? ""
+        
+        if comment.count < 1 {
+            showMsg(msg: "至少寫個字")
+            return
+        }
+        
+        let commentRef = dbReference.childByAutoId()
+        
+        let commentContent = ["commnet":comment,
+                              "user":nickName,
+                              "t":ServerValue.timestamp()] as [String : Any]
+        
+        commentRef.setValue(commentContent)
+        inputTextTF.text = ""
+        
+    }
+    
+    
+    
     
 
 }
